53bf1a2a096841aff451e6090378ee0f79839e66
Alex Zirbel <alex.zirbel@addepar.com>
2015-01-28 10:29:40 -0800

    Add transactions data

---
+++ app/models/transactions.js
@@ -0,0 +1,230 @@
+export default [
+  {
+    date: new Date(2015, 0, 25),
+    description: 'Fiesta Del Mar',
+    category: 'Restaurants',
+    amount: '43.15'
+  },
+  {
+    date: new Date(2015, 0, 24),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '43.23'
+  },
+  {
+    date: new Date(2015, 0, 23),
+    description: 'Chipotle',
+    category: 'Restaurants',
+    amount: '10.25'
+  },
+  {
+    date: new Date(2015, 0, 23),
+    description: 'Shalala',
+    category: 'Restaurants',
+    amount: '35.34'
+  },
+  {
+    date: new Date(2015, 0, 10),
+    description: 'Starbucks',
+    category: 'Restaurants',
+    amount: '9.79'
+  },
+  {
+    date: new Date(2015, 0, 6),
+    description: 'PG&E Monthly',
+    category: 'Utilities',
+    amount: '60.2'
+  },
+  {
+    date: new Date(2015, 0, 4),
+    description: 'Check 102',
+    category: 'Rent',
+    amount: '600'
+  },
+  {
+    date: new Date(2014, 11, 23),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '93'
+  },
+  {
+    date: new Date(2014, 11, 22),
+    description: 'Shalala',
+    category: 'Restaurants',
+    amount: '18.22'
+  },
+  {
+    date: new Date(2014, 11, 21),
+    description: 'Chipotle',
+    category: 'Restaurants',
+    amount: '15.25'
+  },
+  {
+    date: new Date(2014, 11, 16),
+    description: 'Comcast the Devil',
+    category: 'Utilities',
+    amount: '30.56'
+  },
+  {
+    date: new Date(2014, 11, 11),
+    description: 'La Boulange',
+    category: 'Restaurants',
+    amount: '16.26'
+  },
+  {
+    date: new Date(2014, 11, 10),
+    description: 'Starbucks',
+    category: 'Restaurants',
+    amount: '7.79'
+  },
+  {
+    date: new Date(2014, 11, 8),
+    description: 'Whole Foods',
+    category: 'Groceries',
+    amount: '16.09'
+  },
+  {
+    date: new Date(2014, 11, 8),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '56.12'
+  },
+  {
+    date: new Date(2014, 11, 6),
+    description: 'PG&E Monthly',
+    category: 'Utilities',
+    amount: '60.2'
+  },
+  {
+    date: new Date(2014, 11, 4),
+    description: 'Check 101',
+    category: 'Rent',
+    amount: '600'
+  },
+  {
+    date: new Date(2014, 10, 23),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '93'
+  },
+  {
+    date: new Date(2014, 10, 22),
+    description: 'Shalala',
+    category: 'Restaurants',
+    amount: '50.22'
+  },
+  {
+    date: new Date(2014, 10, 21),
+    description: 'Chipotle',
+    category: 'Restaurants',
+    amount: '13.25'
+  },
+  {
+    date: new Date(2014, 10, 16),
+    description: 'Comcast the Devil',
+    category: 'Utilities',
+    amount: '30.56'
+  },
+  {
+    date: new Date(2014, 10, 11),
+    description: 'La Boulange',
+    category: 'Restaurants',
+    amount: '16.26'
+  },
+  {
+    date: new Date(2014, 10, 10),
+    description: 'Starbucks',
+    category: 'Restaurants',
+    amount: '4.79'
+  },
+  {
+    date: new Date(2014, 10, 8),
+    description: 'Whole Foods',
+    category: 'Groceries',
+    amount: '16.09'
+  },
+  {
+    date: new Date(2014, 10, 8),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '56.12'
+  },
+  {
+    date: new Date(2014, 10, 6),
+    description: 'PG&E Monthly',
+    category: 'Utilities',
+    amount: '60.2'
+  },
+  {
+    date: new Date(2014, 10, 4),
+    description: 'Check 100',
+    category: 'Rent',
+    amount: '600'
+  },
+  {
+    date: new Date(2014, 9, 23),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '93'
+  },
+  {
+    date: new Date(2014, 9, 22),
+    description: 'Shalala',
+    category: 'Restaurants',
+    amount: '45.22'
+  },
+  {
+    date: new Date(2014, 9, 21),
+    description: 'Chipotle',
+    category: 'Restaurants',
+    amount: '20.25'
+  },
+  {
+    date: new Date(2014, 9, 16),
+    description: 'Comcast the Devil',
+    category: 'Utilities',
+    amount: '30.56'
+  },
+  {
+    date: new Date(2014, 9, 11),
+    description: 'La Boulange',
+    category: 'Restaurants',
+    amount: '16.26'
+  },
+  {
+    date: new Date(2014, 9, 10),
+    description: 'Starbucks',
+    category: 'Restaurants',
+    amount: '4.79'
+  },
+  {
+    date: new Date(2014, 9, 10),
+    description: 'Chez TJ',
+    category: 'Restaurants',
+    amount: '700'
+  },
+  {
+    date: new Date(2014, 9, 8),
+    description: 'Whole Foods',
+    category: 'Groceries',
+    amount: '16.09'
+  },
+  {
+    date: new Date(2014, 9, 8),
+    description: 'Safeway',
+    category: 'Groceries',
+    amount: '56.12'
+  },
+  {
+    date: new Date(2014, 9, 6),
+    description: 'PG&E Monthly',
+    category: 'Utilities',
+    amount: '60.2'
+  },
+  {
+    date: new Date(2014, 9, 4),
+    description: 'Check 99',
+    category: 'Rent',
+    amount: '600'
+  }
+];
